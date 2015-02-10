53540e9771f5f2750e7135564b3c9f969bfbbb45
CommitEveryday <supermegacoder@gmail.com>
2015-02-10 15:35:26 +0300

    enable trace

--- WebFormsApplication/WebFormsApplication/Properties/AssemblyInfo.cs
+++ WebFormsApplication/WebFormsApplication/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("a7990fe7-324e-458b-84f9-60208cdab83f")]
+[assembly: Guid("34a05dba-cd24-4d0a-a822-51694fcf72da")]
 
 // Version information for an assembly consists of the following four values:
 //

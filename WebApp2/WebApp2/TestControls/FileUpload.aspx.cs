deec0918fd1d31c2093f7ec74ebf2ee87ca4bc5b
CommitEveryday <supermegacoder@gmail.com>
2015-02-22 23:45:58 +0300

    added FileUpload

---
+++ WebApp2/WebApp2/TestControls/FileUpload.aspx.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WebApp2.TestControls
+{
+    public partial class FileUpload : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            if (FileUpload1.HasFile)
+            {
+                Label1.Text = "File length: " + FileUpload1.FileBytes.Length + "<br/>" +
+                    "File name: " + FileUpload1.FileName + "<br/>" +
+                    "MIME type: " + FileUpload1.PostedFile.ContentType;
+                FileUpload1.SaveAs(MapPath("~/Uploads/" + FileUpload1.FileName));
+            }
+            else
+            {
+                Label1.Text = "No file received";
+            }
+        }
+    }
+}

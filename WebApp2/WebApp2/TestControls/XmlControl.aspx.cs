1d2678a6d0f81bc0d6a9e9150ea0e56623c2ea50
CommitEveryday <supermegacoder@gmail.com>
2015-02-26 02:26:53 +0300

    test xml control

---
+++ WebApp2/WebApp2/TestControls/XmlControl.aspx.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WebApp2.TestControls
+{
+    public partial class XmlControl : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            Xml1.DocumentSource = @"~/XmlData/person.xml";
+            Xml1.TransformSource = @"~/XmlData/person.xsl";
+        }
+    }
+}

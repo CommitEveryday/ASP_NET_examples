8a3ec9368dc82dbabb440f5bda1213ed0b686cc8
CommitEveryday <supermegacoder@gmail.com>
2015-03-01 17:50:53 +0300

    test ListBox

---
+++ WebApp2/WebApp2/TestControls/ListBox.aspx
@@ -0,0 +1,24 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="ListBox.aspx.cs" Inherits="WebApp2.TestControls.ListBox" %>
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+    
+        <asp:ListBox ID="ListBox1" runat="server" Height="203px" Width="219px">
+        </asp:ListBox>
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+        <asp:Button ID="Button1" runat="server" onclick="Button1_Click" Text="Button" />
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+        <asp:ListBox ID="ListBox2" runat="server" Height="198px" Width="224px">
+        </asp:ListBox>
+    
+    </div>
+    </form>
+</body>
+</html>
---
+++ WebApp2/WebApp2/TestControls/ListBox.aspx.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WebApp2.TestControls
+{
+    public partial class ListBox : System.Web.UI.Page
+    {
+        private List<Car> carList = Car.GetList();
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            ListBox1.SelectionMode = ListBox2.SelectionMode = ListSelectionMode.Multiple;
+            if (!IsPostBack)
+            {
+                ListBox1.DataSource = carList;
+                ListBox1.DataTextField = "Price";
+                ListBox1.DataValueField = "Price";
+                ListBox1.DataTextFormatString = "Price: {0:C}";
+                ListBox1.DataBind();
+            }
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            foreach (ListItem item in ListBox1.Items)
+            {
+                if (item.Selected)
+                    ListBox2.Items.Add(item);
+            }
+            //ListBox2.DataBind(); //не нужен?
+        }
+    }
+}

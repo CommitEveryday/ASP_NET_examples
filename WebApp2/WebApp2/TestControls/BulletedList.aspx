6aa951aa89e2d0a1890d5740f42ca652ee1228f9
CommitEveryday <supermegacoder@gmail.com>
2015-03-01 19:28:00 +0300

    test BulletedList

---
+++ WebApp2/WebApp2/TestControls/BulletedList.aspx
@@ -0,0 +1,21 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="BulletedList.aspx.cs" Inherits="WebApp2.TestControls.BulletedList" %>
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+        <asp:BulletedList ID="BulletedList1" runat="server">
+        </asp:BulletedList>
+        <asp:ListBox ID="ListBox1" runat="server" 
+            onselectedindexchanged="ListBox1_SelectedIndexChanged"></asp:ListBox>
+        <asp:ListBox ID="ListBox2" runat="server" 
+            onselectedindexchanged="ListBox2_SelectedIndexChanged"></asp:ListBox>
+    </div>
+    </form>
+</body>
+</html>
---
+++ WebApp2/WebApp2/TestControls/BulletedList.aspx.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WebApp2.TestControls
+{
+    public partial class BulletedList : System.Web.UI.Page
+    {
+        private List<Car> carList = Car.GetList();
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            ListBox1.AutoPostBack = ListBox2.AutoPostBack = true;
+            if (!IsPostBack)
+            {
+                BulletedList1.DataSource = carList;
+                BulletedList1.DataTextField = "Make";
+                BulletedList1.DataValueField = "Price";
+                ListBox1.DataSource = Enum.GetNames(typeof(BulletStyle));
+                ListBox2.DataSource = Enum.GetNames(typeof(BulletedListDisplayMode));
+                DataBind();
+            }
+        }
+
+        protected void ListBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            BulletedList1.BulletStyle = (BulletStyle)Enum.Parse(typeof(BulletStyle),
+                ListBox1.SelectedValue);
+        }
+
+        protected void ListBox2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            BulletedList1.DisplayMode = (BulletedListDisplayMode)Enum.Parse(typeof(BulletedListDisplayMode),
+                ListBox2.SelectedValue);
+        }
+    }
+}

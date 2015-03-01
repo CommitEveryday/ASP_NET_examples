b137fc56555eda220de570e7de5d9b9132b2bf0b
CommitEveryday <supermegacoder@gmail.com>
2015-03-01 18:27:30 +0300

    test CheckBoxList and RadioButtonList

---
+++ WebApp2/WebApp2/TestControls/Lists.aspx
@@ -0,0 +1,24 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Lists.aspx.cs" Inherits="WebApp2.TestControls.Lists" %>
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+    
+        <asp:RadioButtonList ID="RadioButtonList1" runat="server">
+        </asp:RadioButtonList>
+        <br />
+        <asp:CheckBoxList ID="CheckBoxList1" runat="server">
+        </asp:CheckBoxList>
+        <br />
+        <asp:ListBox ID="ListBox1" runat="server"></asp:ListBox>
+    
+    </div>
+    </form>
+</body>
+</html>
---
+++ WebApp2/WebApp2/TestControls/Lists.aspx.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WebApp2.TestControls
+{
+    public partial class Lists : System.Web.UI.Page
+    {
+        private List<Car> carList = Car.GetList();
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            RadioButtonList1.RepeatColumns = 3;
+            RadioButtonList1.RepeatDirection = RepeatDirection.Vertical;
+            CheckBoxList1.RepeatColumns = 3;
+            CheckBoxList1.RepeatDirection = RepeatDirection.Horizontal;
+            if (!IsPostBack)
+            {
+                ListBox1.DataSource = carList;
+                ListBox1.DataTextField = "Make";
+                ListBox1.DataValueField = "Price";
+                RadioButtonList1.DataSource = carList;
+                RadioButtonList1.DataTextField = "Make";
+                RadioButtonList1.DataValueField = "Price";
+                CheckBoxList1.DataSource = carList;
+                CheckBoxList1.DataTextField = "Make";
+                CheckBoxList1.DataValueField = "Price";
+                DataBind();
+            }
+        }
+    }
+}

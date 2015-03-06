c40f264146b70aa74b6262088d98f3d5eda6bdad
CommitEveryday <supermegacoder@gmail.com>
2015-03-07 00:50:36 +0300

    add data class Customer

---
+++ WebApp2/WebApp2/UsingDataBoundControls/Customer.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApp2.UsingDataBoundControls
+{
+    public class Customer
+    {
+        public Int32 Id { get; set; }
+        public String Name { get; set; }
+        public String City { get; set; }
+        public String State { get; set; }
+        public String Phone { get; set; }
+
+        public Customer()
+        { }
+
+        public Customer(Int32 Id, String Name, String City, String State, String Phone)
+        {
+            this.Id = Id;
+            this.Name = Name;
+            this.City = City;
+            this.State = State;
+            this.Phone = Phone;
+        }
+    }
+}
---
+++ WebApp2/WebApp2/UsingDataBoundControls/CustomerList.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApp2.UsingDataBoundControls
+{
+    public class CustomerList
+    {
+        private static List<Customer> custList = new List<Customer>();
+
+        public List<Customer> Select()
+        {
+            return custList;
+        }
+
+        public void Update(Customer updateCustomer)
+        {
+            Customer customerFound = custList.FirstOrDefault<Customer>(c => c.Id == updateCustomer.Id);
+            if (customerFound == null)
+                return;
+            customerFound.Name = updateCustomer.Name;
+            customerFound.City = updateCustomer.City;
+            customerFound.State = updateCustomer.State;
+            customerFound.Phone = updateCustomer.Phone;
+        }
+
+        public void Insert(Customer customer)
+        {
+            custList.Add(customer);
+        }
+
+        public void Delete(Customer deleteCustomer)
+        {
+            custList.RemoveAll(c => c.Id == deleteCustomer.Id);
+        }
+
+        public Customer SelectSingle(Int32 id)
+        {
+            if (id == -1)
+                return null;
+            return custList.FirstOrDefault<Customer>(c => c.Id == id);
+        }
+    }
+}

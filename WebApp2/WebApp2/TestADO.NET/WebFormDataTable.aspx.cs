58a228e80382b499d2f7f4ad4674f9cdfa11a7a3
CommitEveryday <supermegacoder@gmail.com>
2015-03-09 23:06:12 +0300

    start learning ADO.NET. DataTable.

---
+++ WebApp2/WebApp2/TestADO.NET/WebFormDataTable.aspx.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data;
+
+namespace WebApp2.TestADO.NET
+{
+    public partial class WebFormDataTable : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private DataTable GetDataTable()
+        {
+            DataTable employee = new DataTable("Employee");
+
+            return employee;
+        }
+    }
+}

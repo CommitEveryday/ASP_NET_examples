f0642db2d87ac6342581e4ce9faf262f81abc251
CommitEveryday <supermegacoder@gmail.com>
2015-04-12 01:57:40 +0300

    End Disconnected class test

---
+++ WebApp2/WebApp2/TestADO.NET/WorkingWithDisconnectedData.aspx
@@ -0,0 +1,18 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WorkingWithDisconnectedData.aspx.cs" Inherits="WebApp2.TestADO.NET.WorkingWithDisconnectedData" %>
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+    <asp:GridView id="GridView" runat="server"
+        Style="z-index: 100; left: 55px; position:absolute; top:15px">
+    </asp:GridView>
+    </div>
+    </form>
+</body>
+</html>
---
+++ WebApp2/WebApp2/TestADO.NET/WorkingWithDisconnectedData.aspx.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WebApp2.TestADO.NET
+{
+    public partial class WorkingWithDisconnectedData : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                Sales salesDataSet = new Sales();
+                Enumerable.Range(0, 10).ToList<int>().ForEach(x =>
+                    salesDataSet.Customer.Rows.Add(Guid.NewGuid(), "Acme" + x));
+                GridView.DataSource = salesDataSet;
+                GridView.DataMember = "Customer";
+                DataBind();
+            }
+        }
+    }
+}
